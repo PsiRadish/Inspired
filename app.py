9e6d348c1747c31881ef13d75a10047535b0f9cb
PsiRadish <psiradish@comcast.net>
2015-09-14 16:12:03 -0700

    Prep for early heroku run.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    #app.run()
